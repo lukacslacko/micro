15774cd2f5b87617e436aedbded80d64e66aefe4
t <lukacslacko@gmail.com>
2023-05-21 10:36:47 +0200

    Debug.

--- tetris.c
+++ tetris.c
@@ -10,12 +10,12 @@
 #define I2C_PIN PIND
 #define I2C_CLOCK (1<<4)
 #define I2C_DATA (1<<5)
-#define ERROR_LED (1<<3)
-#define OK_LED (1<<2)
+// #define ERROR_LED (1<<3)
+// #define OK_LED (1<<2)
 #define true 1
 #define false 0
-#define SHOW_NEXT 1
-#define USE_HOLD 1
+#define SHOW_NEXT true
+#define USE_HOLD true
 
 typedef char bool;
 typedef uint8_t u8;
@@ -58,9 +58,9 @@ void data_release() {
 void i2c_init() {
 	data_release();
 	clock_release();
-	I2C_DDR |= OK_LED | ERROR_LED;
+	/*I2C_DDR |= OK_LED | ERROR_LED;
 	I2C_PORT |= OK_LED;
-	I2C_PORT &= ~ERROR_LED;
+	I2C_PORT &= ~ERROR_LED;*/
 }
 
 void i2c_start() {
@@ -73,11 +73,11 @@ void i2c_stop() {
 	data_release();
 }
 
-void i2c_fail() {
+/*void i2c_fail() {
 	I2C_PORT &= ~OK_LED;
 	I2C_PORT |= ERROR_LED;
 	while (1);
-}
+}*/
 
 void i2c_send(uint8_t data) {
 	uint8_t mask = 0x80;
@@ -90,7 +90,7 @@ void i2c_send(uint8_t data) {
 	}
 	data_release();
 	clock_release();
-	if (I2C_PIN & I2C_DATA) i2c_fail();
+	if (I2C_PIN & I2C_DATA) while(1);
 	clock_low();
 }
 
@@ -112,26 +112,22 @@ void range(char colmin, char colmax, char rowmin, char rowmax) {
 }
 
 void tetris_get_next();
-void tetris_init();
 void tetris_move_up();
 bool tetris_check(u16);
-void tetris_left();
-void tetris_right();
-void tetris_rot1();
-void tetris_rot2();
 void tetris_fall();
-void tetris_hold();
-void tetris_end();
 
-const u16 pieces[] PROGMEM = {0x0660,0x8888,0x0710,0x0170,0x06c0,0x0c60,0x4640,0,0x0660,0xf000,0x0226,0x0446,0x4620,0x2640,0x0720,0,0x0660,0x1111,0x08e0,0x0e80,0x0360,0x0630,0x0262,0,0x0660,0x000f,0x6220,0x6440,0x0462,0x0264,0x04e0,0};
+const u16 pieces[] PROGMEM = {0x0660,0x8888,0x0710,0x0170,0x06c0,0x0c60,0x4640,0,0x0660,0xf000,0x0226,0x0446,0x4620,0x2640,0x0720,0,0x0660,0x1111,0x08e0,0x0e80,0x0360,0x0630,0x0262,0,0x0660,0x000f,0x6440,0x6220,0x0462,0x0264,0x04e0,0};
 
-u8 rand() {
-	seed = 58566*seed+27443;
-	return (seed*53458)>>8;
+void rand() {
+	seed ^= seed << 7;
+    seed ^= seed >> 9;
+    seed ^= seed << 8;
 }
 
 i8 rand7() {
-	return rand()%7;
+	rand();
+	while ((seed&7)==7) rand();
+	return seed&7;
 }
 
 void tetris_get_next() {
@@ -156,7 +152,7 @@ void tetris_xor() {
 	u16 t = piece;
 	for ( i8 y = 0; y < 4; y++) {
 		for ( i8 x = 0; x < 4; x++) {
-			if ( t&0x80 ) {
+			if ( t&0x8000 ) {
 				set_board(posx+x,posy+y,!get_board(posx+x,posy+y));
 			}
 			t <<= 1;
@@ -164,22 +160,11 @@ void tetris_xor() {
 	}
 }
 
-void tetris_init() {
-	for (u8 i = 0; i < 25; i++) { board[i] = 0; }
-	hold = 0;
-	tetris_get_next();
-	piece = next;
-	pi = ni;
-	tetris_get_next();
-	tetris_move_up();
-	lines_cleared = 0;
-}
-
 void tetris_move_up() {
 	posy = 0;
 	posx = 3;
 	if (!tetris_check(piece)) {
-		tetris_end();
+		while(1);
 	}
 }
 
@@ -199,7 +184,7 @@ void tetris_clear_lines() {
 	while (ltc) {
 		cl = 0;
 		i8 l = 0;
-		while (cl!=0x3ff) {
+		while ((cl&0x3ff)!=0x3ff) {
 			for ( i8 i = 0; i < 10; i++ ) {
 				cl <<= 1;
 				cl += get_board(i,l);
@@ -230,49 +215,10 @@ bool tetris_check(u16 p) {
 	return true;
 }
 
-void tetris_left() {
-	posx--;
-	if (!tetris_check(piece)) {
-		posx++;
-	}
-}
-
-void tetris_right() {
-	posx++;
-	if (!tetris_check(piece)) {
-		posx--;
-	}
-}
-
-void tetris_rot1() {
-	/*u16 new = ((piece<<12)&32768)|((piece<<7)&16384)|((piece<<2)&8192)|((piece>>3)&4096)|((piece<<9)&2048)|((piece<<4)&1024)|((piece>>1)&512)|((piece>>5)&256)|((piece<<6)&128)|((piece<<1)&64)|((piece>>4)&32)|((piece>>9)&16)|((piece<<3)&8)|((piece>>2)&4)|((piece>>7)&2)|((piece>>12)&1);
-	if (tetris_check(new)) {
-		piece = new;
-	}*/
-	pi += 8;
-	if (tetris_check(pieces[pi&31])) {
-		piece = pieces[pi&32];
-	} else {
-		pi -= 8;
-	}
-}
-
-void tetris_rot2() {
-	/*u16 new = ((piece<<3)&32768)|((piece<<6)&16384)|((piece<<9)&8192)|((piece<<12)&4092)|((piece>>2)&2048)|((piece<<1)&1024)|((piece<<4)&512)|((piece<<7)&256)|((piece>>7)&128)|((piece>>4)&64)|((piece>>1)&32)|((piece<<2)&16)|((piece>>12)&8)|((piece>>9)&4)|((piece>>6)&2)|((piece>>3)&1);
-	if (tetris_check(new)) {
-		piece = new;
-	}*/
-	pi -= 8;
-	if (tetris_check(pieces[pi&31])) {
-		piece = pieces[pi&32];
-	} else {
-		pi += 8;
-	}
-}
-
 void tetris_fall() {
 	posy++;
 	if (!tetris_check(piece)) {
+		posy--;
 		tetris_xor();
 		piece = next;
 		pi = ni;
@@ -282,32 +228,7 @@ void tetris_fall() {
 	}
 }
 
-void tetris_instant_drop() {
-	do {tetris_fall();} while (posy);
-}
-
-void tetris_hold() {
-#if USE_HOLD
-	if (!hold) {
-		hold = piece;
-		hi = pi;
-		piece = next;
-		pi = ni;
-		tetris_get_next();
-		tetris_move_up();
-		return;
-	}
-	u16 t = piece;
-	u8 ti = pi;
-	piece = hold;
-	pi = hi;
-	hold = t;
-	hi = ti;
-	tetris_move_up();
-#endif
-}
-
-void tetris_draw_board() {
+void tetris_draw() {
 	tetris_xor();
 	range(36,116,3,7);
 	for ( i8 x = 9; x > 0; x -= 2 ) {
@@ -319,29 +240,6 @@ void tetris_draw_board() {
 		}
 	}
 	tetris_xor();
-}
-
-void tetris_draw_next() {
-#if SHOW_NEXT
-	u16 t = pieces[(ni+8)&31];
-	range(8,23,1,2);
-	for ( i8 i = 0; i < 2; i++ ) { for ( i8 j = 0; j < 4; j++ ) {
-		u8 p = (t&0x8000?0xf:0)|(t&0x800?0xf0:0);
-		for ( i8 k = 0; k < 4; k++ ) { i2c_send(p); }
-		t <<= 1;
-	} t <<= 4; }
-	/*u16 t = next;
-	range(42,74,2,5);
-	for ( i8 i = 0; i < 16; i++ ) {
-		for ( i8 j = 0; j < 8; j ++ ) {
-			i2c_send(t&0x80?0xff:0x00);
-		}
-		t <<= 1;
-	}*/
-#endif
-}
-
-void tetris_draw_hold() {
 	u16 t = pieces[(hi+8)&31];
 	range(8,23,5,6);
 	for ( i8 i = 0; i < 2; i++ ) { for ( i8 j = 0; j < 4; j++ ) {
@@ -349,38 +247,35 @@ void tetris_draw_hold() {
 		for ( i8 k = 0; k < 4; k++ ) { i2c_send(p); }
 		t <<= 1;
 	} t <<= 4; }
-	/*u16 t = hold;
-	range(82,114,2,5);
-	for ( i8 i = 0; i < 16; i++ ) {
-		for ( i8 j = 0; j < 8; j ++ ) {
-			i2c_send(t&0x80?0xff:0x00);
-		}
+#if SHOW_NEXT
+	t = pieces[(ni+8)&31];
+	range(8,23,1,2);
+	for ( i8 i = 0; i < 2; i++ ) { for ( i8 j = 0; j < 4; j++ ) {
+		u8 p = (t&0x8000?0xf:0)|(t&0x800?0xf0:0);
+		for ( i8 k = 0; k < 4; k++ ) { i2c_send(p); }
 		t <<= 1;
-	}*/
-}
-
-void tetris_draw() {
-	tetris_draw_board();
-	tetris_draw_next();
-	tetris_draw_hold();
-}
-
-void tetris_end() {
-	while (true) {}
-}
-
-void set_timer(char kilocycles) {
-	TCCR0A = 0x02; // Count up to kilocycles.
-	TCCR0B = 0x05; // Timer clock every 1024 cycles.
-	OCR0A = kilocycles;
+	} t <<= 4; }
+#endif
 }
 
 int main() {
+	TCCR0A = 0x02; // Count up to kilocycles.
+	TCCR0B = 0x05; // Timer clock every 1024 cycles.
+	DDRB = 0xf;
+	PORTB = 0xf7;
 	i2c_init();
 	i2c_seq(oled_init, sizeof(oled_init));
-	//for (int i = 0; i < 128*8; ++i) i2c_send(0);
+	for (int i = 0; i < 128*8; ++i) i2c_send(0);
 	seed = 4;
-	tetris_init();
+	for (u8 i = 0; i < 25; i++) { board[i] = 0; }
+	hold = 0;
+	hi = 7;
+	tetris_get_next();
+	piece = next;
+	pi = ni;
+	tetris_get_next();
+	tetris_move_up();
+	lines_cleared = 0;
 /*	tetris_draw();
 	tetris_left();
 	tetris_right();
@@ -390,14 +285,102 @@ int main() {
 	tetris_hold();
 	tetris_instant_drop();
 	*/
-	set_timer(100);
-	I2C_DDR = OK_LED | ERROR_LED;
-	I2C_PORT = OK_LED;
+	i8 time = 0;
+	OCR0A = 100;
+	u8 prev_pressed_buttons;
+	u8 pressed_buttons = 0;
 	while (true) {
+		tetris_draw();
+		prev_pressed_buttons = pressed_buttons;
+		pressed_buttons = 0;
+		PORTB = 0xf7;
+		if (!(PINB&0x10)) { // left
+			pressed_buttons |= 0x08;
+		}
+		if (!(PINB&0x20)) { // right
+			pressed_buttons |= 0x40;
+		}
+		if (!(PINB&0x40)) { // rot2
+			pressed_buttons |= 0x20;
+		}
+		if (!(PINB&0x80)) { // rot1
+			pressed_buttons |= 0x10;
+		}
+		PORTB = 0xfb;
+		if (!(PINB&0x10)) { // instant drop
+			pressed_buttons |= 0x80;
+		}
+		if (!(PINB&0x20)) { // fast drop
+			pressed_buttons |= 0x04;
+		}
+		if (!(PINB&0x40)) { // nothing
+			pressed_buttons |= 0x02;
+		}
+		if (!(PINB&0x80)) { // hold
+			pressed_buttons |= 0x01;
+		}
+		prev_pressed_buttons ^= pressed_buttons;
+		prev_pressed_buttons &= pressed_buttons;
+		if (prev_pressed_buttons&0x80) {
+			posx--;
+			if (!tetris_check(piece)) {
+				posx++;
+			}
+		}
+		if (prev_pressed_buttons&0x40) {
+			posx++;
+			if (!tetris_check(piece)) {
+				posx--;
+			}
+		}
+		if (prev_pressed_buttons&0x20) {
+			pi -= 8;
+			if (tetris_check(pieces[pi&31])) {
+				piece = pieces[pi&31];
+			} else {
+				pi += 8;
+			}
+		}
+		if (prev_pressed_buttons&0x10) {
+			pi += 8;
+			if (tetris_check(pieces[pi&31])) {
+				piece = pieces[pi&31];
+			} else {
+				pi -= 8;
+			}
+		}
+		if (prev_pressed_buttons&0x08) {
+			do{tetris_fall();}while(posy);
+		}
+		if (prev_pressed_buttons&0x01) {
+#if USE_HOLD
+			if (!hold) {
+				hold = piece;
+				hi = pi;
+				piece = next;
+				pi = ni;
+				tetris_get_next();
+				tetris_move_up();
+			} else {
+				u16 t = piece;
+				u8 ti = pi;
+				piece = hold;
+				pi = hi;
+				hold = t;
+				hi = ti;
+				tetris_move_up();
+			}
+#endif
+		}
 		if (TIFR & 1) {
 			TIFR |= 1;
-			I2C_PORT ^= ERROR_LED;
-			I2C_PORT ^= OK_LED;
+			// I2C_PORT ^= OK_LED|ERROR_LED;
+			time++;
+			OCR0A = 100-lines_cleared/10;
+			if (time>((pressed_buttons&0x04)?10:50)) {
+				tetris_fall();
+				time -= (pressed_buttons&0x04)?10:50;
+			}
 		}
 	}
 }
